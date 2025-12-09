7dc8212748e357e7b2feb54a1d5d570dd6f6d14a
choedo <96doyeong@naver.com>
2025-12-09 22:16:58 +0900

    연락처 기본 데이터 추가

---
+++ src/data/contact.ts
@@ -0,0 +1,31 @@
+export const EMAIL_DOMAINS = [
+  'naver.com',
+  'gmail.com',
+  'daum.net',
+  'kakao.com',
+  'nate.com',
+  'outlook.com',
+  'icloud.com',
+] as const;
+
+export const MOBILE_PREFIXES = [
+  '010',
+  '011',
+  '016',
+  '017',
+  '018',
+  '019',
+] as const;
+
+export const CONTACT_PREFIXES = [
+  '02',
+  '031',
+  '032',
+  '042',
+  '051',
+  '052',
+  '053',
+  '055',
+  '062',
+  '064',
+] as const;
