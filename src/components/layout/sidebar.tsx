fe8b06c6248d7578a976f1edf355601a5a24dcec
choedo <96doyeong@naver.com>
2026-02-20 12:59:21 +0900

    로고 백그라운드 제거 및 다크모드 로고 추가

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,14 +1,32 @@
 import Logo from '@/assets/logo.png';
+import DarkLogo from '@/assets/logo-dark.png';
 import ProjectList from '@/components/project/project-list';
 import { Link } from 'react-router';
+import { useTheme } from '@/store/theme';
+import React from 'react';
 
 export default function Sidebar() {
+  const theme = useTheme();
+
+  const [logo, setLogo] = React.useState(Logo);
+
+  React.useEffect(() => {
+    if (theme === 'system') {
+      const isDarkMode = window.matchMedia(
+        '(prefers-color-scheme: dark)',
+      ).matches;
+      setLogo(isDarkMode ? DarkLogo : Logo);
+    } else {
+      setLogo(theme === 'dark' ? DarkLogo : Logo);
+    }
+  }, [theme]);
+
   return (
     <div className={`max-w-50 w-full border-r flex flex-col`}>
       <div className={`h-[56px] border-b`}>
         <Link to={'/'}>
           <img
-            src={Logo}
+            src={logo}
             alt={'mockstack logo'}
             className={`object-contain h-full m-auto`}
           />
